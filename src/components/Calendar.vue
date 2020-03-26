2d213514e5ba796bdf05369384753feda3474670
Alexander Masud <alexandermasud@gmail.com>
2020-03-26 08:14:10 +0100

    Pasted in calendar from vuetify

---
+++ src/components/Calendar.vue
@@ -0,0 +1,165 @@
+<template>
+  <v-row class="fill-height">
+    <v-col>
+      <v-sheet height="64">
+        <v-toolbar flat color="white">
+          <v-btn color="primary" dark @click.stop="dialog = true">
+            New Event
+          </v-btn>
+          <v-btn outlined class="mr-4" @click="setToday">
+            Today
+          </v-btn>
+          <v-btn fab text small @click="prev">
+            <v-icon small>mdi-chevron-left</v-icon>
+          </v-btn>
+          <v-btn fab text small @click="next">
+            <v-icon small>mdi-chevron-right</v-icon>
+          </v-btn>
+
+          <v-toolbar-title>{{ title }}</v-toolbar-title>
+
+          <div class="flex-grow-1"></div>
+          <v-mnpm audit enu bottom right>
+            <template v-slot:activator="{ on }">
+              <v-btn outlined v-on="on">
+                <span>{{ typeToLabel[type] }}</span>
+                <v-icon right>mdi-menu-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="type = 'day'">
+                <v-list-item-title>Day</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'week'">
+                <v-list-item-title>Week</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'month'">
+                <v-list-item-title>Month</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = '4day'">
+                <v-list-item-title>4 days</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-mnpm>
+        </v-toolbar>
+      </v-sheet>
+
+      <v-dialog v-model="dialog" max-width="500">
+        <v-card>
+          <v-container>
+            <v-form @submit.prevent="addEvent">
+              <v-text-field
+                v-model="name"
+                type="text"
+                label="name"
+              ></v-text-field>
+              <v-text-field
+                v-model="details"
+                type="text"
+                label="detail"
+              ></v-text-field>
+              <v-text-field
+                v-model="start"
+                type="date"
+                label="start"
+              ></v-text-field>
+              <v-text-field
+                v-model="end"
+                type="date"
+                label="end"
+              ></v-text-field>
+              <v-text-field
+                v-model="color"
+                type="color"
+                label="color"
+              ></v-text-field>
+              <v-btn
+                type="submit"
+                color="primary"
+                class="mr-4"
+                @click.stop="dialog = false"
+              >
+                submit
+              </v-btn>
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-dialog>
+
+      <v-sheet height="600">
+        <v-calendar
+          ref="calendar"
+          v-model="focus"
+          color="primary"
+          :events="events"
+          :event-color="getEventColor"
+          :event-margin-bottom="3"
+          :now="today"
+          :type="type"
+          @click:event="showEvent"
+          @click:more="viewDay"
+          @click:date="viewDay"
+          @change="updateRange"
+        ></v-calendar>
+        <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          full-width
+          offset-x
+        >
+          <v-card color="grey lighten-4" :width="350" flat>
+            <v-toolbar :color="selectedEvent.color" dark>
+              <v-btn @click="deleteEvent(selectedEvent.id)" icon>
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+              <div class="flex-grow-1"></div>
+            </v-toolbar>
+
+            <v-card-text>
+              <form v-if="currentlyEditing !== selectedEvent.id">
+                {{ selectedEvent.details }}
+              </form>
+              <form v-else>
+                <textarea-autosize
+                  v-model="selectedEvent.details"
+                  type="text"
+                  style="width: 100%"
+                  :min-height="100"
+                  placeholder="add note"
+                >
+                </textarea-autosize>
+              </form>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn text color="secondary" @click="selectedOpen = false">
+                close
+              </v-btn>
+              <v-btn
+                v-if="currentlyEditing !== selectedEvent.id"
+                text
+                @click.prevent="editEvent(selectedEvent)"
+              >
+                edit
+              </v-btn>
+              <v-btn
+                text
+                v-else
+                type="submit"
+                @click.prevent="updateEvent(selectedEvent)"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
+      </v-sheet>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAdMviRMVEMfrPLBJhpVb_dOLEjWS1CCtg",
+  authDomain: "letsplan-db6bc.firebaseapp.com",
+  databaseURL: "https://letsplan-db6bc.firebaseio.com",
+  projectId: "letsplan-db6bc",
+  storageBucket: "letsplan-db6bc.appspot.com",
+  messagingSenderId: "409869151092",
+  appId: "1:409869151092:web:f0492c6b4560e518c2da21",
+  measurementId: "G-DRXGVDGNZS"
+});
+
+export const db = firebase.firestore();
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
